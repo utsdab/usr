eef125f7cdecc7406a1c292837dbe0cc29668762
mgidney <mgidney@gmail.com>
2019-08-08 17:48:38 +1000

    fixed small bugs tractor export

--- etc/maya/dab_scripts/batch_render_rfm.py
+++ etc/maya/dab_scripts/batch_render_rfm.py
@@ -1,7 +1,7 @@
 # This script if launched from a maya shelf button
 # it is a wrapper for submitting RFM batch jobs to the farm.
 #
-import maya_tools.uts_tools.rfm_tractor2 as rfm2
+
 
 def run():
     try:
@@ -10,6 +10,7 @@ def run():
         print("Failed to import module: {}".format(ie))
     else:
         rfm2.batch_render_spool(do_bake=False)
+
 
 if __name__ == "__main__":
     print "RUNNING Batch Render Rfm ...."
