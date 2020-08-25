df82b6e1fd2ff086aa01c70b58e8c2aafc937ce7
mgidney <mgidney@gmail.com>
2020-08-25 22:14:39 +1000

    update batch

--- etc/maya/dab_scripts/batch_render_camseq_rfm.py
+++ etc/maya/dab_scripts/batch_render_camseq_rfm.py
@@ -5,13 +5,10 @@
 def run():
     try:
         import maya_tools.uts_tools.mg_camseq_render as mgs
-
     except ImportError as ie:
         print("Failed to import module: {}".format(ie))
-
     else:
         mgs.run()
-
 
 if __name__ == "__main__":
     print "RUNNING Batch Render Rfm ...."
